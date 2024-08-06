332a851431c99db6e43c5c2126126f6c6cfffeb4
Muhammed Aman <muhammedaman2.oaman@gmail.com>
2024-08-06 20:12:06 +0530

    .add

--- Task-Router/Week3-Task2/Task2/src/Pages/Home.jsx
+++ Task-Router/Week3-Task2/Task2/src/Pages/Home.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 function Home() {
   return (
     <div>
-      <h1>Welcome to the Blog Page</h1>
+      <h1>Welcome to the Blog Page </h1>
       <p><Link to='/createblog'>Create a New Blog</Link></p>
       <p><Link to='/blogs'>View All Blogs</Link></p>
     </div>
